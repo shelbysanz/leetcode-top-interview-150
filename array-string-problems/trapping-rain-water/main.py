af2d745dd6e15cba00bafb9a25f98b643d161d02
Shelby Sanchez-Herrera <shelbsgt500@gmail.com>
2025-11-23 22:08:24 -0500

    completed trapping rain water (optimized O(n) time and O(1) space)

--- array-string-problems/trapping-rain-water/main.py
+++ array-string-problems/trapping-rain-water/main.py
@@ -1,26 +1,24 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
+        if not height:
+            return 0
         total_count = 0
-        while True:
-            counting: bool = False
-            count = 0
-            should_break = True
-            for i in height:
-                if i:
-                    if count:
-                        total_count += count
-                        count = 0
-                    counting = True
-                    should_break = False
-                elif i == 0:
-                    if counting:
-                        count += 1
-            if should_break:
-                break
-            height = [i - 1 if i > 0 else 0 for i in height]
+        start = 0
+        end = len(height) - 1
+        start_max = height[start]
+        end_max = height[end]
+        while start < end:
+            if height[start] <= height[end]:
+                start += 1
+                start_max = max(start_max, height[start])
+                total_count += start_max - height[start]
+            else:
+                end -= 1
+                end_max = max(end_max, height[end])
+                total_count += end_max - height[end]
         return total_count
 
 
 if __name__ == "__main__":
-    # assert Solution().trap([4,2,0,3,2,5]) == 9
+    assert Solution().trap([4,2,0,3,2,5]) == 9
     assert Solution().trap([4, 2, 3]) == 1
