e4b0784d3008898deb8466f831cbc8ad1df804a1
Shelby Sanchez-Herrera <shelbsgt500@gmail.com>
2025-12-01 20:40:33 -0500

    refactor: extracted TreeNode into a seperate class file I can reference - I only use this for linting purposes

--- binary-tree-general-problems/maximum-depth-of-binary-tree/main.py
+++ binary-tree-general-problems/maximum-depth-of-binary-tree/main.py
@@ -1,11 +1,4 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(
-        self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None
-    ):
-        self.val: int = val
-        self.left: TreeNode | None = left
-        self.right: TreeNode | None = right
+from ..TreeNode import TreeNode
 
 
 class Solution:
