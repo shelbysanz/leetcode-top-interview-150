858e092321f02f4d4694e5c20d81e080a9bfcd1a
shelbysanz <shelbsgt500@gmail.com>
2025-12-22 16:52:42 -0500

    completed implement trie (golang)

--- trie-problems/go-implement-trie/main.go
+++ trie-problems/go-implement-trie/main.go
@@ -1,4 +1,6 @@
-package leetcode
+package main
+
+import "fmt"
 
 type Trie struct {
 	children map[byte]*Trie
@@ -11,21 +13,47 @@ func Constructor() Trie {
 
 func (this *Trie) Insert(word string) {
 	curr := this
-	for _, b := range word {
-		val, ok := curr.children[byte(b)]
+	for i := 0; i < len(word); i++ {
+		b := word[i]
+		val, ok := curr.children[b]
 		if !ok {
 			val = &Trie{children: make(map[byte]*Trie)}
-			curr.children[byte(b)] = val
+			curr.children[b] = val
 		}
 		curr = val
 	}
 	curr.isLeaf = true
 }
 
+func (this *Trie) find(word string) (*Trie, bool) {
+	curr := this
+	for i := 0; i < len(word); i++ {
+		b := word[i]
+		val, ok := curr.children[b]
+		if !ok {
+			return nil, false
+		}
+		curr = val
+	}
+	return curr, true
+}
+
 func (this *Trie) Search(word string) bool {
-	return false
+	curr, found := this.find(word)
+	return curr != nil && curr.isLeaf && found
 }
 
 func (this *Trie) StartsWith(prefix string) bool {
-	return false
+	_, found := this.find(prefix)
+	return found
+}
+
+func main() {
+	trie := Constructor()
+	trie.Insert("apple")
+	fmt.Println("search apple:", trie.Search("apple"))
+	fmt.Println("search app:", trie.Search("app"))
+	fmt.Println("starts with app:", trie.StartsWith("app"))
+	trie.Insert("app")
+	fmt.Println("search app:", trie.Search("app"))
 }
