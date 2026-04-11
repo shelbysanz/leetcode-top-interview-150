a5107c1406e583f0b5900cda24dc8966a60f47e2
shelbysanz <shelbsgt500@gmail.com>
2026-04-10 21:51:34 -0400

    completed invert binary tree

---
+++ ggp-problems/easy_invert_binary_tree.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+        return root
