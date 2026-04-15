52f5ca96b0f08f8b1eea5d3f06aca8c47fbb7ab5
Shelby Sanchez <shelby@bluearrowmail.com>
2026-04-14 22:59:24 -0400

    completed longest palindromic subsequence

---
+++ ggp-problems/medium_longest_palindromic_subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        def lcs(string1, string2):
+            # initialize dp
+            n = len(s)
+            dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+            for i in range(n - 1, -1, -1):
+                for j in range(n - 1, -1, -1):
+                    if string1[i] == string2[j]:
+                        dp[i][j] = 1 + dp[i + 1][j + 1]
+                    else:
+                        dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+            return dp[0][0]
+
+        return lcs(s, s[::0])
