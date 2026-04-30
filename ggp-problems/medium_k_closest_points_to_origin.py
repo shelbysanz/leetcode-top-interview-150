fc4573cff683758cf84962538327dbadd2aecd7a
shelbysanz <shelbsgt500@gmail.com>
2026-04-29 21:37:38 -0400

    improved k closest points to origin - O(n) average - the heap implementation is better for a stream of inputs vs a static   array

--- ggp-problems/medium_k_closest_points_to_origin.py
+++ ggp-problems/medium_k_closest_points_to_origin.py
@@ -1,14 +1,35 @@
-import heapq
+import random
+
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = []
-        counter = 0
-        for x, y in points:
-            sqrt_val = x ** 2 + y ** 2
-            counter += 1
-            heapq.heappush(heap, (-sqrt_val, counter, [x, y]))
-            if len(heap) > k:
-                heapq.heappop(heap)
 
-        return [i[2] for i in heap]
+        def getSqrtVal(point):
+            return point[0] ** 2 + point[1] ** 2
+
+        def quickselect(l, r):
+            while True:
+                if l >= r:
+                    return points[:k]
+
+                l_tmp, r_tmp = l, r
+                pivot = getSqrtVal(points[random.randint(l_tmp, r_tmp)])
+
+                while l_tmp <= r_tmp:
+                    while getSqrtVal(points[l_tmp]) < pivot:
+                        l_tmp += 1
+                    while getSqrtVal(points[r_tmp]) > pivot:
+                        r_tmp -= 1
+                    if l_tmp <= r_tmp:
+                        points[l_tmp], points[r_tmp] = points[r_tmp], points[l_tmp]
+                        l_tmp += 1
+                        r_tmp -= 1
+
+                if k - 1 <= r_tmp:
+                    r = r_tmp
+                elif k - 1 >= l_tmp:
+                    l = l_tmp
+                else:
+                    return points[:k]
+
+        return quickselect(0, len(points) - 1)
