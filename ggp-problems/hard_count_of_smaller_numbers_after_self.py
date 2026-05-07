49502bedf47dd25e65936fec92b57c39196c1fb5
shelbysanz <shelbsgt500@gmail.com>
2026-05-06 22:41:02 -0400

    improved count of smaller numbers after self (fenwick tree)

--- ggp-problems/hard_count_of_smaller_numbers_after_self.py
+++ ggp-problems/hard_count_of_smaller_numbers_after_self.py
@@ -1,12 +1,25 @@
-from sortedcontainers import SortedList
-
-
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        seen, output = SortedList([]), [0] * n
-        for i in range(n - 1, -1, -1):
-            idx = seen.bisect_left(nums[i])
-            output[i] = idx
-            seen.add(nums[i])
+        values = sorted(set(nums))
+        rank_map = {v: i + 1 for i, v in enumerate(values)}
+        fenwick = [0] * (len(values) + 1)
+        output = [0] * len(nums)
+
+        def update(i):
+            while i < len(fenwick):
+                fenwick[i] += 1
+                i += i & -i
+
+        def query(i):
+            total = 0
+            while i > 0:
+                total += fenwick[i]
+                i -= i & -i
+            return total
+
+        for i in range(len(nums) - 1, -1, -1):
+            rank = rank_map[nums[i]]
+            output[i] = query(rank - 1)
+            update(rank)
+
         return output
