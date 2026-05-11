a8192483220f75a2cebfecfc385cdb5a8fbc9088
shelbysanz <shelbsgt500@gmail.com>
2026-05-10 23:04:36 -0400

    completed candy

---
+++ ggp-problems/hard_candy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = 0
+        n = len(ratings)
+        left, right = [1] * n, [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                right[i] = right[i + 1] + 1
+        for i in range(n):
+            candies += max(left[i], right[i])
+        return candies
