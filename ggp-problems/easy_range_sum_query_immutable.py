6e8c33b3a14aa9e841e903078d49110d5970c1f7
shelbysanz <shelbsgt500@gmail.com>
2026-05-12 21:12:50 -0400

    completed range sum query immutable

---
+++ ggp-problems/easy_range_sum_query_immutable.py
@@ -0,0 +1,16 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix_sum = [0] * len(nums)
+        self.generate(nums)
+
+    def generate(self, nums):
+        self.prefix_sum[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.prefix_sum[i] = self.prefix_sum[i - 1] + nums[i]
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.prefix_sum[right]
+
+        return self.prefix_sum[right] - self.prefix_sum[left - 1]
